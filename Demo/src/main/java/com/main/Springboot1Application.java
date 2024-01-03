890cf0a3dc4153fa311a310dd1a1d1996108d475
arjun-agnisys <100676401+yatendra322@users.noreply.github.com>
2024-01-03 23:33:23 +0530

    create project

---
+++ Demo/src/main/java/com/main/Springboot1Application.java
@@ -0,0 +1,14 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot1Application.class, args);
+		System.out.println("hiiiiiiiiiiiiii");
+	}
+
+}
