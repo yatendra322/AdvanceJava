fc6057f0816c478d2abd935530843245522e8124
arjun-agnisys <100676401+yatendra322@users.noreply.github.com>
2024-01-07 17:48:03 +0530

    create project SpringMVC Boot and test by jsp file using RequestMapping

---
+++ SpringMVCBoot/src/main/java/com/main/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping("/contact")
+	public String contact() {
+		return "contact";
+	}
+}
